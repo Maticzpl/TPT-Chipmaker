fc7d24e9c808ff6f56bb25b8a344daf4415b7ae7
Maticzpl <czerneckimateusz0@gmail.com>
2023-07-25 00:28:38 +0200

    Previous commit broke smth else

--- src/2 utils.lua
+++ src/2 utils.lua
@@ -285,9 +285,10 @@ function MaticzplChipmaker.ReorderParticles()
                 local x = math.floor(i % sim.XRES)
                 local y = math.floor((i - x) / sim.XRES)
 
-                local id = sim.partCreate(-3,x,y,part.type)
 
-                sim.partProperty(id,'type',part.type); -- Jacob1's mod had troubles with this :P
+                local id = sim.partCreate(-3,x,y,elem.DEFAULT_PT_DMND)
+
+                sim.partProperty(id,'type',part.type);
                 sim.partProperty(id,'temp',part.temp);
                 sim.partProperty(id,'ctype',part.ctype);
                 sim.partProperty(id,'tmp',part.tmp);
